5b37749378de16623a97d0524c22e16afb430cc4
LaNMaSteR53 <tjt1980@gmail.com>
2016-02-28 03:06:40 -0500

    Added some custom middleware to handle reverse proxy headers.

---
+++ sharepass/middleware.py
@@ -0,0 +1,10 @@
+class ReverseProxied(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
